7a2720c240cc5ba7493362919a385294eb1080ec
SergioAlfaroLahoz <sergio.alfaro-ext@bshg.com>
2020-02-25 17:15:19 +0100

    Task List

---
+++ js/main.js
@@ -0,0 +1,49 @@
+(function(){
+    // Variables
+    var list = document.getElementById("list");
+    var textInput = document.getElementById("textInput");
+    var button = document.getElementById("buttonAdd");
+
+    //Functions
+    var addTask = function(){
+        var task = textInput.value;
+        var newTask = document.createElement("li");
+        var link = document.createElement("a");
+        var content = document.createTextNode(task);
+
+        if(task==""){
+            textInput.setAttribute("placeholder","Add a valid task");
+            textInput.className = "error";
+            return false;
+        }
+
+        link.appendChild(content); //Add content to link
+        link.setAttribute("href", "#"); //Add link to the new a
+        newTask.appendChild(link);
+        list.appendChild(newTask);
+
+        textInput.value = ""; //Clean text input
+
+        for(var i=0; i<list.children.length;i++){
+            list.children[i].addEventListener("click", function(){
+                this.parentNode.removeChild(this);
+            });
+        }
+    };
+
+    var checkInput = function(){
+        textInput.className = "";
+        textInput.setAttribute("placeholder", "Add a task");
+    };
+
+    var deleteTask = function(){
+        this.parentNode.removeChild(this);
+    };
+
+    //Events
+    button.addEventListener("click", addTask);
+    textInput.addEventListener("click", checkInput);
+    for(var i=0; i<list.children.length;i++){
+        list.children[i].addEventListener("click", deleteTask);
+    }
+}());
